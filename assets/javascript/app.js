d7d203ed902ab21ffc65d5e6e0856c116d6fd885
JamilW <jamilweeks@yahoo.com>
2019-01-13 18:58:03 -0500

    updated multi choice answers in HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,42 @@
-var timeLeft = 60;
+var timeLeft = 30;
 var elem = document.getElementById("timer");
 var timerId = setInterval(countdown, 1000);
 var $questionContent = $("<div></div>");
 var questions = [
     {question: "Which rap group is not a member of the Native Tongues?",
-     choices: ["A Tribe Called Quest", "Wu-Tang Clan", "De La Soul", "Jungle Brothers"],
+     choices1: ["A Tribe Called Quest", "Wu-Tang Clan", "De La Soul", "Jungle Brothers"],
      answer: 1
     },
     {question: "First Rap Album to Go Platinum?",
-     choices: ["License to Ill", "Straight Outta Compton", "Straight Outta Compton", "Raising Hell"],
+     choices2: ["License to Ill", "Straight Outta Compton", "Paid In Full", "Raising Hell"],
      answer: 3
     },
     {question: "Which rap group was the first rap artists to get a million dollar record deal?",
-     choices: ["Eric B. & Rakim", "Wu-Tang Clan", "Public Enemy", "N.W.A."],
+     choices3: ["Eric B. & Rakim", "Wu-Tang Clan", "Public Enemy", "N.W.A."],
      answer: 0
     },
     {question: "Who is the highest selling rap artist of all time?",
-     choices: ["Jay-Z", "2Pac", "Eminem", "MC Hammer"],
+     choices4: ["Jay-Z", "2Pac", "Eminem", "MC Hammer"],
      answer: 2
     },
     {question: "Is the debut of rap artist, Nas, with the album, Illmatic, considered a classic?",
-     choices: ["True", "False"],
+     choices5: ["True", "False"],
      answer: 0
     },
     {question: "Which album did not sell over 10 million copies?",
-     choices: ["The Blueprint", "The Marshall Mathers LP", "All Eyez On Me", "Life After Death"],
+     choices6: ["The Blueprint", "The Marshall Mathers LP", "All Eyez On Me", "Life After Death"],
      answer: 0
     },
     {question: "Who was the first rap artist(s) to win a Grammy?",
-     choices: ["MC Hammer", "LL Cool J", "Run DMC", "DJ Jazzy Jeff and the Fresh Prince"],
+     choices7: ["MC Hammer", "LL Cool J", "Run DMC", "DJ Jazzy Jeff & the Fresh Prince"],
      answer: 3
    },
     {question: "Jay-Z beat Nas in their rap battle",
-     choices: ["True", "False"],
+     choices8: ["True", "False"],
      answer: 1
     },
 ]
+var currentQuestion
 var correctAnswers;
 var incorrectAnswers;
 var unanswered;
@@ -62,23 +63,35 @@ function countdown() {
     }
 }
 
-function quiz()   {
-   // $("#q1").html(questions.question1);
-   // $("#q2").html(questions.question2);
-   // $("#q3").text(questions.question3).show();
-   // $("#q4").text(questions.question4).show();
-   // $("#q5").text(questions.question5).show();
-   // $("#q6").text(questions.question6).show();
-   // $("#q7").text(questions.question7).show();
-   // $("#q8").text(questions.question8).show();
-
-   for(var i=0; i < questions.length; i++) {
-
-      var el = "#q" + (i +1);
-
-      $(el).prepend("<div>" + questions[i].question + "</div>");
-
-   }
+function submitButton() {
+   $("#submit").on("click", function() {
+      $("#results").show();
+      $(".quiz").hide();
+   });
 }
-quiz();
+submitButton();
+
+function quiz()   {
+   for(var i = 0; i < questions.length; i++) {
+      var el = "#q" + (i + 1);
+      $(el).prepend("<div>" + questions[i].question + "</div>"); 
+      }
+      for(var j = 0; j < questions.length; j++)   {
+         console.log(questions);
+         var e1 = ".answer1" + (j + 1);
+         var e2 = ".answer2" + (j + 1);
+         var e3 = ".answer3" + (j + 1);
+         var e4 = ".answer4" + (j + 1);
+         var e_array = [e1, e2, e3, e4];
+     //    for (var i = 0; i < questions[j].choices.length; i++) {   
+            $(e_array[i]).text("<label>" + "<input>" + "A: " + questions.indexOf("A Tribe Called Quest") + "</label>" + "<br>");
+      //      $(e_array[i]).html(questions[j].choices);
+  //          console.log(questions[j].choices[k]);
+         }
+         }
+         
+      
+      quiz();
+
+     
 
